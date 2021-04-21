18f891087c485f3e9cc9b53463eb26fb8bdf6f74
Jeff Saupe <saupe.jeff@gmail.com>
2021-04-21 16:18:36 +0200

    Remove not needed links

--- src/main/java/de/saupe/jeff/timetable/utils/Settings.java
+++ src/main/java/de/saupe/jeff/timetable/utils/Settings.java
@@ -3,8 +3,5 @@ package de.saupe.jeff.timetable.utils;
 public class Settings {
     public static String centuria;
     public static String semester;
-
-    public static final String URL_LOGIN = "https://cis.nordakademie.de/?no_cache=1";
-    public static final String URL_STUDY_PLAN_HTML = "https://cis.nordakademie.de/fileadmin/Infos/Stundenplaene/" + "%s" + "_" + "%s" + ".html";
     public static final String URL_STUDY_PLAN_ICS = "https://cis.nordakademie.de/fileadmin/Infos/Stundenplaene/" + "%s" + "_" + "%s" + ".ics";
 }
