b00771c0173d0b136f0844e27bfb3b71892b5ea8
Jeff Saupe <saupe.jeff@gmail.com>
2021-05-03 21:43:16 +0200

    Update Properties.java

--- src/main/java/de/saupe/jeff/schedulecleaner/utils/Properties.java
+++ src/main/java/de/saupe/jeff/schedulecleaner/utils/Properties.java
@@ -4,6 +4,5 @@ public class Properties {
     public static final String NAME = "ScheduleCleaner";
     public static final String VERSION = "1.0";
     public static final String BANNER = "banner.txt";
-
     public static final String URL_ICS = "https://cis.nordakademie.de/fileadmin/Infos/Stundenplaene/" + "%s" + "_" + "%s" + ".ics";
 }
