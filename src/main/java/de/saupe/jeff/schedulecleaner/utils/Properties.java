68d56e3c11faef1eaa14fb21084e6a15ebaf4112
Jeff Saupe <saupe.jeff@gmail.com>
2021-05-12 11:05:02 +0200

    Increment version

--- src/main/java/de/saupe/jeff/schedulecleaner/utils/Properties.java
+++ src/main/java/de/saupe/jeff/schedulecleaner/utils/Properties.java
@@ -2,7 +2,7 @@ package de.saupe.jeff.schedulecleaner.utils;
 
 public class Properties {
     public static final String NAME = "ScheduleCleaner";
-    public static final String VERSION = "1.1.5";
+    public static final String VERSION = "1.1.6";
     public static final String BANNER = "banner.txt";
     public static final String URL_ICS = "https://cis.nordakademie.de/fileadmin/Infos/Stundenplaene/" + "%s" + "_" + "%s" + ".ics";
     public static final String PATH_DYNAMIC_ICS_SERVER = "/cleaned-schedule/";
