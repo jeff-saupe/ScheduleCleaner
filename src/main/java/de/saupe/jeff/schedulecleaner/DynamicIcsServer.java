0f0e1ab99f558af3385d7c5483e14d4d7acd903c
Yannick Brandt <yannick-brandt@outlook.de>
2021-05-12 23:16:17 +0200

    Fixed CORS issue for zoomCalendar

--- src/main/java/de/saupe/jeff/schedulecleaner/DynamicIcsServer.java
+++ src/main/java/de/saupe/jeff/schedulecleaner/DynamicIcsServer.java
@@ -47,6 +47,7 @@ public class DynamicIcsServer implements HttpHandler {
     private static void sendResponse(HttpExchange exchange, int statusCode, String contentType, String response) {
         try (OutputStream outputStream = exchange.getResponseBody()) {
             exchange.getResponseHeaders().add("Content-Type", contentType);
+            exchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
             System.out.println(response);
             byte[] responseBytes = response.getBytes(StandardCharsets.ISO_8859_1);
             exchange.sendResponseHeaders(statusCode, responseBytes.length);
