7f2ab9c58d5a0cc7cee5a561d9b6c3f719cb9a92
Jeff Saupe <saupe.jeff@gmail.com>
2021-05-16 01:06:53 +0200

    Update FixResponse.java

--- src/main/java/de/saupe/jeff/schedulecleaner/fixes/FixResponse.java
+++ src/main/java/de/saupe/jeff/schedulecleaner/fixes/FixResponse.java
@@ -4,8 +4,8 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public enum FixResponse {
-    TOO_MANY_PARAMETERS("Too many parameters found"),
-    TOO_FEW_PARAMETERS("Too few parameters found"),
+    TOO_MANY_PARAMETERS("Too many parameters"),
+    TOO_FEW_PARAMETERS("Too few parameters"),
     OK ("OK");
 
     private final String name;
