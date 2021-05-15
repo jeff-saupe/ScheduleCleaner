ad8e5b03edbe1604ff610b5ad56595412a252276
Jeff Saupe <saupe.jeff@gmail.com>
2021-05-16 01:07:44 +0200

    Implement URL filtering #5

--- src/main/java/de/saupe/jeff/schedulecleaner/environment/impl/DynamicIcsServer.java
+++ src/main/java/de/saupe/jeff/schedulecleaner/environment/impl/DynamicIcsServer.java
@@ -7,19 +7,26 @@ import de.saupe.jeff.schedulecleaner.ResponseHandler;
 import de.saupe.jeff.schedulecleaner.ScheduleCleaner;
 import de.saupe.jeff.schedulecleaner.ScheduleCleaner.CleaningAction;
 import de.saupe.jeff.schedulecleaner.environment.Environment;
+import de.saupe.jeff.schedulecleaner.fixes.Fix;
+import de.saupe.jeff.schedulecleaner.fixes.FixFactory;
+import de.saupe.jeff.schedulecleaner.fixes.FixResponse;
 import de.saupe.jeff.schedulecleaner.misc.Properties;
 import lombok.extern.log4j.Log4j2;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Log4j2
 public class DynamicIcsServer extends Environment implements HttpHandler {
-    private final Pattern URLPattern = Pattern.compile(".*/([a-zA-Z][0-9]{2}[a-zA-Z])_([0-9])\\.ics");
+    private final Pattern URLPattern = Pattern.compile(".*/([a-zA-Z][0-9]{2}[a-zA-Z])_([0-9])\\.ics(\\?(.*)|)");
+    private final Pattern fixPattern = Pattern.compile("(.*)=(.*)");
 
     @Override
     public void start() {
@@ -45,20 +52,21 @@ public class DynamicIcsServer extends Environment implements HttpHandler {
         String path = exchange.getRequestURI().getPath();
 
         // Check URL
-        Matcher matcher = URLPattern.matcher(path);
-        if (!matcher.find()) {
+        Matcher urlMatcher = URLPattern.matcher(path);
+        if (!urlMatcher.find()) {
             sendBadURLResponse(exchange);
             return;
         }
 
         // Check centuria and semester
-        String centuria = matcher.group(1);
-        String semester = matcher.group(2);
+        String centuria = urlMatcher.group(1);
+        String semester = urlMatcher.group(2);
         if (centuria == null || semester == null) {
             sendBadURLResponse(exchange);
             return;
         }
 
+        // Create cleaner
         ScheduleCleaner cleaner = new ScheduleCleaner(centuria, semester, CleaningAction.CLEAN);
         cleaner.setResponseHandler(new ResponseHandler() {
             @Override
@@ -71,7 +79,69 @@ public class DynamicIcsServer extends Environment implements HttpHandler {
                 sendResponse(exchange, 404, "text", message);
             }
         });
-        cleaner.clean();
+
+        String tail = urlMatcher.group(4);
+        try {
+            // Read fixes if existent
+            List<Fix> fixes = readFixes(tail);
+
+            // Add fixes
+            fixes.forEach(cleaner::addFix);
+
+            // Start cleaner
+            cleaner.clean();
+        } catch (IllegalArgumentException exception) {
+            sendBadRequestResponse(exchange, exception.getMessage());
+        }
+
+    }
+
+    private List<Fix> readFixes(String url) throws IllegalArgumentException {
+        List<Fix> fixes = new ArrayList<>();
+
+        if (url != null && !url.isEmpty()) {
+            if (StringUtils.countMatches(url, "?") != 0) {
+                throw new IllegalArgumentException("Wrong syntax: Too many question marks.");
+            } else {
+                String[] parameters = url.split("&");
+
+                // Iterate over all possible fixes
+                for(String parameter : parameters) {
+                    Matcher fixMatcher = fixPattern.matcher(parameter);
+
+                    if (!fixMatcher.find()) {
+                        throw new IllegalArgumentException("Wrong syntax for fix: " + parameter);
+                    } else {
+                        // Possible fix found
+                        String fixName = fixMatcher.group(1);
+
+                        if (fixName != null) {
+                            try {
+                                // Create fix
+                                Fix fix = FixFactory.createFix(fixName);
+
+                                String fixValue = fixMatcher.group(2);
+                                if (fixValue != null) {
+                                    String[] values = fixValue.split(";");
+
+                                    // Insert fix' parameters and validate them
+                                    FixResponse response = fix.setParameters(values);
+                                    if (response == FixResponse.OK) {
+                                        fixes.add(fix);
+                                    } else {
+                                        throw new IllegalArgumentException(response.toString() + " for " + parameter);
+                                    }
+                                }
+                            } catch (IllegalArgumentException exception) {
+                                throw new IllegalArgumentException("Fix not found: " + fixName);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return fixes;
     }
 
     private static void sendBadURLResponse(HttpExchange exchange) {
