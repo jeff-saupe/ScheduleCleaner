59232622993052c286ee25710a5175b8f1173510
Yannick Brandt <yannick-brandt@outlook.de>
2021-05-17 01:57:02 +0200

    Added frontent closes #5

---
+++ frontend/src/ExcludeList.tsx
@@ -0,0 +1,79 @@
+import {
+	Button,
+	Divider,
+	IconButton,
+	List,
+	ListItem,
+	ListItemIcon,
+	ListItemSecondaryAction,
+	ListSubheader,
+	TextField,
+} from "@material-ui/core";
+import { v4 as uuid } from "uuid";
+import Filter from "./FilterIcon";
+import React from "react";
+import { Add, Delete } from "@material-ui/icons";
+import validate from "./validate";
+import useStyles from "./ScheduleConfigurationStyles";
+
+export default function ExcludeList(props: {
+	exclude: { id: string; text: string }[];
+	setExclude: any;
+}) {
+	const classes = useStyles();
+	return (
+		<List
+			subheader={
+				<ListSubheader className={classes.listSubheader}>
+					Exclude subjects that contain
+					<Button
+						variant="outlined"
+						startIcon={<Add />}
+						onClick={(_) =>
+							props.setExclude([...props.exclude, { id: uuid(), text: "" }])
+						}
+					>
+						Add
+					</Button>
+				</ListSubheader>
+			}
+		>
+			{props.exclude.map((excludeItem, index) => (
+				<React.Fragment key={excludeItem.id}>
+					<ListItem>
+						<ListItemIcon>
+							<Filter />
+						</ListItemIcon>
+						<TextField
+							margin="dense"
+							variant="outlined"
+							value={excludeItem.text}
+							error={!validate.excludeText(excludeItem.text)}
+							label="text"
+							fullWidth
+							className={classes.listInput}
+							onChange={(e) => {
+								const copy = [...props.exclude];
+								copy[index].text = e.target.value;
+								props.setExclude(copy);
+							}}
+						></TextField>
+						<ListItemSecondaryAction>
+							<IconButton
+								edge="end"
+								onClick={(e) => {
+									const copy = [...props.exclude];
+									copy.splice(index, 1);
+									props.setExclude(copy);
+								}}
+							>
+								<Delete />
+							</IconButton>
+						</ListItemSecondaryAction>
+					</ListItem>
+					<Divider className={classes.divider} />
+				</React.Fragment>
+			))}
+		</List>
+	);
+}
---
+++ frontend/src/ReplaceList.tsx
@@ -0,0 +1,97 @@
+import {
+	Button,
+	Divider,
+	IconButton,
+	List,
+	ListItem,
+	ListItemIcon,
+	ListItemSecondaryAction,
+	ListSubheader,
+	TextField,
+} from "@material-ui/core";
+import { v4 as uuid } from "uuid";
+import validate from "./validate";
+import React from "react";
+import { Add, Delete, Edit } from "@material-ui/icons";
+import useStyles from "./ScheduleConfigurationStyles";
+
+export default function ReplaceList(props: {
+	replace: { before: string; after: string; id: string }[];
+	setReplace: any;
+}) {
+	const classes = useStyles();
+	return (
+		<List
+			subheader={
+				<ListSubheader className={classes.listSubheader}>
+					Replace subject titles
+					<Button
+						variant="outlined"
+						startIcon={<Add />}
+						onClick={(_) =>
+							props.setReplace([
+								...props.replace,
+								{ before: "", after: "", id: uuid() },
+							])
+						}
+					>
+						Add
+					</Button>
+				</ListSubheader>
+			}
+		>
+			{props.replace.map((replaceItem, index) => (
+				<React.Fragment key={replaceItem.id}>
+					<ListItem>
+						<ListItemIcon>
+							<Edit />
+						</ListItemIcon>
+
+						<TextField
+							margin="dense"
+							variant="outlined"
+							label="Before"
+							error={!validate.replaceText(replaceItem.before)}
+							onChange={(e) => {
+								const copy = [...props.replace];
+								copy[index].before = e.target.value;
+								props.setReplace(copy);
+							}}
+						>
+							{replaceItem.before}
+						</TextField>
+						<div style={{ marginRight: 10 }}></div>
+						<TextField
+							margin="dense"
+							variant="outlined"
+							label="After"
+							className={classes.listInput}
+							error={!validate.replaceText(replaceItem.after)}
+							onChange={(e) => {
+								const copy = [...props.replace];
+								copy[index].after = e.target.value;
+								props.setReplace(copy);
+							}}
+						>
+							{replaceItem.after}
+						</TextField>
+
+						<ListItemSecondaryAction>
+							<IconButton
+								edge="end"
+								onClick={(e) => {
+									const copy = [...props.replace];
+									copy.splice(index, 1);
+									props.setReplace(copy);
+								}}
+							>
+								<Delete />
+							</IconButton>
+						</ListItemSecondaryAction>
+					</ListItem>
+					<Divider className={classes.divider} />
+				</React.Fragment>
+			))}
+		</List>
+	);
+}
